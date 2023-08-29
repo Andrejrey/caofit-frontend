9de2aa0fb8a345d270f1a13d2f9e3ed68074b8a6
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-08-29 10:12:44 +0200

    fix messages

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,14 @@ function App() {
   const [food, setFood] = useState([]);
   const [isCartModalOpen, setCartModalOpen] = useState(false);
 
-  console.log(food && food);
-
   useEffect(() => {
-    axios.get("http://localhost:8080/foodlist").then((response) => {
+    axios.get("http://localhost:8080/food_list").then((response) => {
       setFood(response.data);
     });
   }, []);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/shopitems").then((response) => {
+    axios.get("http://localhost:8080/shopi_tems").then((response) => {
       setShopItems(response.data);
     });
   }, []);
