3aa4867350b7447b89c0631366869f590772a3fe
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-09-04 19:44:06 +0200

    create user profile, loading and page not found component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,22 @@
 const NotFound = () => {
   return (
-    <div>
-      <h1>Hey, you look lost</h1>
+    <div className="h-screen flex flex-col justify-center items-center bg-homeBg bg-cover bg-top">
+      <div className="flex p-1 items-center  text-white text-4xl font-semibold bg-dark-blue-light rounded-lg">
+        <div className="flex p-3  flex-col justify-center items-center text-dark-blue-light rounded-lg">
+          <p className="font-semibold text-first">Hey, you look lost</p>
+          <div className="mt-5 flex flex-row items-center">
+            <img
+              src="src\assets\logo\caoFit_light_logo_without_text.svg"
+              className="w-36"
+              alt="logo"
+            />
+            <p className="font-bold text-first text-7xl ml-5">404</p>
+          </div>
+          <p className="font-semibold text-first">
+            This page doesn't seem to exist
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -6,43 +6,79 @@ import shopping from "../assets/icons/shopping-cart.png";
 
 const UserProfile = ({ user }) => {
   return user ? (
-    <div className="flex h-screen flex-col justify-center items-center mt-5 mb-5">
-      <h1>
-        Name:{" "}
-        <span className="text-dark-blue-light font-bold">
-          {user.first_name}
-        </span>
-      </h1>
-      <div className="flex flex-col items-center mt-3">
-        <img src={strong} alt="Muscle Icon" className="w-12 mb-2 self-center" />
-        <Link
-          className="transform rounded-lg w-fit self-center bg-first px-6 py-3 font-semibold text-dark-blue shadow-md transition duration-300 hover:scale-105 hover:bg-yellow-400"
-          to={"/calculator"}
-        >
-          Calculator
-        </Link>
-        <img
-          src={book}
-          alt="Muscle Icon"
-          className="w-12 mb-2 self-center mt-2"
-        />
-        <Link
-          className="transform rounded-lg w-fit self-center bg-first px-6 py-3 font-semibold text-dark-blue shadow-md transition duration-300 hover:scale-105 hover:bg-yellow-400"
-          to={"/diary"}
-        >
-          Diary
-        </Link>
-        <img
-          src={shopping}
-          alt="Muscle Icon"
-          className="w-12 mb-2 self-center mt-2"
-        />
-        <Link
-          className="transform rounded-lg w-fit self-center bg-first px-6 py-3 font-semibold text-dark-blue shadow-md transition duration-300 hover:scale-105 hover:bg-yellow-400"
-          to={"/shop"}
-        >
-          Shop
-        </Link>
+    <div className="flex h-screen flex-col justify-center items-start bg-homeBg bg-cover bg-top">
+      <div className="text-center text-dark-blue mx-3 mb-10 lg:w-3/5 xl:w-1/2 xl:ml-10 2xl:w-1/3 2xl:ml-80">
+        <h1 className="text-3xl">
+          Name:{" "}
+          <span className="text-dark-blue-light font-bold">
+            {user.first_name}
+          </span>
+        </h1>
+        <div className="flex flex-col items-center mt-3">
+          <img
+            src={strong}
+            alt="Muscle Icon"
+            className="w-12 mb-2 self-center mt-5"
+          />
+          <h2 className="font-semibold">
+            Empower Your Nutrition - Track and Thrive!
+          </h2>
+          <hr className="h-px my-1 mx-10 bg-dark-blue border-0"></hr>
+          <p className="mt-3 text-base px-3">
+            Discover a new way of food tracking with our revolutionary app!
+            Effortlessly monitor what you eat and uncover the secrets of your
+            eating habits. Gain valuable insights into your food intake, from
+            calories to micronutrients.
+          </p>
+          <Link
+            className="transform mt-5 rounded-lg w-fit self-center bg-first px-6 py-3 font-semibold text-dark-blue shadow-md transition duration-300 hover:scale-105 hover:bg-yellow-400"
+            to={"/calculator"}
+          >
+            Calculator
+          </Link>
+          <img
+            src={book}
+            alt="Muscle Icon"
+            className="w-12 mb-2 self-center mt-5"
+          />
+          <h2 className="font-semibold">
+            Nutrition Diary - Your Journey of Empowerment
+          </h2>
+          <hr className="h-px my-1 mx-10 bg-dark-blue border-0"></hr>
+          <p className="mt-3 text-base px-3">
+            Transform your health with CaoFIT's nutrition tracking. Get accurate
+            insights into your diet. Make informed choices for weight management
+            or overall wellness. Visualize progress, spot patterns, and optimize
+            nutrition effortlessly.
+          </p>
+          <Link
+            className="transform mt-5 rounded-lg w-fit self-center bg-first px-6 py-3 font-semibold text-dark-blue shadow-md transition duration-300 hover:scale-105 hover:bg-yellow-400"
+            to={"/diary"}
+          >
+            Diary
+          </Link>
+          <img
+            src={shopping}
+            alt="Muscle Icon"
+            className="w-12 mb-2 self-center mt-5"
+          />
+          <h2 className="font-semibold">
+            Ultimate Destination for Health Enthusiasts
+          </h2>
+          <hr className="h-px my-1 mx-10 bg-dark-blue border-0"></hr>
+          <p className="mt-3 text-base px-3">
+            Discover an array of curated selection of top-tier protein products,
+            which fuel your workouts or support your recovery. Step into the
+            world of CaoFIT Shop and embrace the power of protein
+            supplementation!
+          </p>
+          <Link
+            className="transform mt-5 rounded-lg w-fit self-center bg-first px-6 py-3 font-semibold text-dark-blue shadow-md transition duration-300 hover:scale-105 hover:bg-yellow-400"
+            to={"/shop"}
+          >
+            Shop
+          </Link>
+        </div>
       </div>
     </div>
   ) : (
